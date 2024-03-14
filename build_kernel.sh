e7f3ffa02d23edef900f0a818d98098e9f88c49f
DeadPool-4422 <dynamicplayer444@gmail.com>
2024-03-14 20:46:32 +0530

    initial commmit for kernel source

---
+++ build_kernel.sh
@@ -0,0 +1,14 @@
+SRC_DIR=$(pwd)
+TC_DIR=$(pwd)/toolchain/
+JOBS=$(nproc)
+MAKE_PARAMS="-j$JOBS -C $SRC_DIR O=$SRC_DIR/out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 CROSS_COMPILE=$TC_DIR/bin/llvm-"
+export PATH="$TC_DIR/bin:$PATH"
+make $MAKE_PARAMS vendor/lineage-a52sxq_defconfig
+make $MAKE_PARAMS
+
+
+# Compile requirements
+# toolchain - https://android.googlesource.com/platform//prebuilts/clang/host/linux-x86/+/refs/heads/android11-qpr3-release/clang-r383902b1/
+
+# commands  for gcc(on debian)
+# sudo apt-get install gcc-aarch64-linux-gnu -y
